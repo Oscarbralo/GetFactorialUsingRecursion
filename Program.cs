0f1aee9c7e7cef3c67c5a581ebd9c029c50b9902
Oscarbralo <oscarbralo@outlook.com>
2013-10-29 10:36:27 +0100

    added getFactorialUsingRecursion

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GetFactorialWithRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial f = new Factorial();
+            long result = f.getFactorial(20);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+
+    public class Factorial
+    {
+        public long getFactorial(long x)
+        {
+            if (x == 1)
+                return x;
+            x *= getFactorial(x - 1);
+            return x;
+        }
+    }
+}
